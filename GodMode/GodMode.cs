8d23a242f06f5cff7802fecb302f11935a19dbf9
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-01-14 00:00:24 +0100

    ?

---
+++ GodMode/GodMode.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GodMode
+{
+    private ManualLogSource mls = Logger.CreateLogSource("fenicemaster.GodMode");
+    [BepInPlugin("fenicemaster.GodMode", "GodMode", "1.0.0")]
+    public class GodMode
+    {
+    }
+}
