fd683a4d7ffb7656ce3567df204d016af8391b9e
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-01-14 12:50:47 +0100

    plugin

---
+++ GodMode/Patcher.cs
@@ -0,0 +1,19 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System.Runtime.CompilerServices;
+
+namespace GodMode
+{
+    internal class Patcher
+    {
+        [HarmonyPatch(typeof(PlayerControllerB), "AllowPlayerDeath")]
+        [HarmonyPrefix]
+        private static bool NoDie() => false;
+
+        [HarmonyPatch(typeof(PlayerControllerB), "Update")]
+        [HarmonyPostfix]
+        private static void update(ref int ___health) {
+            ___health = 1000;
+        }
+    }
+}
---
+++ GodMode/Plugins.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace GodMode
+{
+    [BepInPlugin("fenicemaster.GodMode", "GodMode", "1.0.0")]
+    public class GodMode : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("fenicemaster.GodMode");
+        public static GodMode instance;
+        internal ManualLogSource mls;
+        void Awake()
+        {
+            if (instance == null) instance = this;
+            mls = BepInEx.Logging.Logger.CreateLogSource("fenicemaster.GodMode");
+            harmony.PatchAll(typeof(GodMode));
+            harmony.PatchAll(typeof(Patcher));
+            mls.LogInfo("GodMode is loaded");
+        }
+    }
+}
