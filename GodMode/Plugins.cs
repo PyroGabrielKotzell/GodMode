d8f40c540a6a7ec6817267201d28e1813b9aeb78
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-01-31 13:11:51 +0100

    get base configs

--- GodMode/Plugins.cs
+++ GodMode/Plugins.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Configuration;
 using HarmonyLib;
 
 namespace GodMode
@@ -10,9 +11,15 @@ namespace GodMode
         private readonly Harmony harmony = new Harmony("fenicemaster.GodMode");
         public static GodMode instance;
         internal ManualLogSource mls;
+        public static ConfigEntry<bool> immortality;
+        public static ConfigEntry<bool> health_patch;
+        public static ConfigEntry<int> health_number;
 
         void Awake()
         {
+            GodMode.immortality = base.Config.Bind<bool>("GodMode", "Disable death", true, "Disable player death.");
+            GodMode.health_patch = base.Config.Bind<bool>("GodMode", "Modify health", true, "Every update the health is set to the specified amount.");
+            GodMode.health_number = base.Config.Bind<int>("GodMode", "Health amount", 1000, "The amount of health to have");
             if (instance == null) instance = this;
             mls = BepInEx.Logging.Logger.CreateLogSource("fenicemaster.GodMode");
             harmony.PatchAll(typeof(GodMode));
